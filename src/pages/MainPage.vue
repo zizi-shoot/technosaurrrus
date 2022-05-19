50d2c09ab54e39995a59a73b14e227532558aa40
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2022-05-19 21:04:18 +0300

    extract preloader and error loading to components

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -16,33 +16,8 @@
         :price-to.sync="filterPriceTo"
       />
       <section class="catalog">
-        <div v-if="isProductLoading" class="loading">
-          <svg
-            height="128px"
-            preserveAspectRatio="xMidYMid"
-            style="margin: auto; background-image: none; display: block; shape-rendering: auto;"
-            viewBox="0 0 100 100"
-            width="128px"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="50" cy="50" fill="none" r="28" stroke="#9efe02"
-                    stroke-dasharray="43.982297150257104 43.982297150257104" stroke-linecap="round" stroke-width="7">
-              <animateTransform attributeName="transform" dur="0.9900990099009901s" keyTimes="0;1" repeatCount="indefinite"
-                                type="rotate" values="0 50 50;360 50 50"></animateTransform>
-            </circle>
-            <circle cx="50" cy="50" fill="none" r="20" stroke="#272727"
-                    stroke-dasharray="31.41592653589793 31.41592653589793" stroke-dashoffset="31.41592653589793"
-                    stroke-linecap="round"
-                    stroke-width="7">
-              <animateTransform attributeName="transform" dur="0.9900990099009901s" keyTimes="0;1" repeatCount="indefinite"
-                                type="rotate" values="0 50 50;-360 50 50"></animateTransform>
-            </circle>
-          </svg>
-        </div>
-        <div v-if="hasErrorLoading" class="error">
-          <span class="error__descr">Произошла ошибка при загрузке товаров!</span>
-          <button class="button button--primary" type="button" @click="loadProducts">Попробовать еще раз</button>
-        </div>
+        <BasePreloader v-if="isProductLoading" />
+        <BaseErrorLoading v-else-if="hasErrorLoading" descr="Произошла ошибка при загрузке товаров!" @load-again="loadProducts" />
         <ProductList :products="products" />
         <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
       </section>
@@ -56,6 +31,8 @@ import ProductList from '@/components/ProductList.vue';
 import ProductFilter from '@/components/ProductFilter.vue';
 import axios from 'axios';
 import { API_URL } from '@/config';
+import BasePreloader from '@/components/BasePreloader.vue';
+import BaseErrorLoading from '@/components/BaseErrorLoading.vue';
 
 export default {
   data() {
@@ -72,6 +49,8 @@ export default {
     };
   },
   components: {
+    BaseErrorLoading,
+    BasePreloader,
     ProductList,
     BasePagination,
     ProductFilter,
@@ -146,28 +125,4 @@ export default {
 .catalog {
   position: relative;
 }
-
-.loading,
-.error {
-  position: absolute;
-  z-index: 1000;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(255 255 255 / 70%);
-}
-
-.error__descr {
-  font-weight: 500;
-  color: #f00;
-}
-
-.button--primary:hover {
-  background-color: #272727;
-}
-
 </style>
